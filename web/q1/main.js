2f20fad76dc8e3499a02469ea28601f06633e4d2
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-12-22 16:51:13 +0800

    moved project by lang; added js version for question 1;

---
+++ web/q1/main.js
@@ -0,0 +1,28 @@
+var c = 10;
+
+// this is a comment
+// the a the second line
+var name = "Beeno";
+'A' == 65; // this is true
+
+/*this is multiple
+ line comment*/
+
+var sum = 0;
+for (var i = 1; i < 1000; i = i + 1) {
+    if ((i % 3) == 0)
+        sum = sum + i;
+    else if ((i % 5) == 0)
+        sum = sum + i;
+}
+
+console.log(sum);
+
+
+/* other code example */
+if (i % 3 == 0)
+    if (i % 5 == 0)
+        sum += i;
+
+// sum = sum + 1
+sum++;
