de54996fa9e9a608a4d6f85c69c6b24d9bdcbbbb
Beeno Tung <aabbcc1241@yahoo.com.hk>
2016-12-29 13:37:55 +0800

    finished question 2 in haskell

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -15,3 +15,8 @@ count acc n sum =
                                    then acc+sum
                                    else sum
                          )
+
+q2 = sum $ filter even $ takeWhile (<4000000) fib
+
+fib :: [Int]
+fib = 1:2:(zipWith (+) fib (tail fib))
